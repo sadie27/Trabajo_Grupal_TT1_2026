15dc40e32c5c7214b190fb7b538e85f768aa19ae
Lucas Alonso <39613949+lucasaf04@users.noreply.github.com>
2026-04-07 16:50:27 +0200

    openapi generated

---
+++ src/main/java/org/openapitools/model/ProblemDetails.java
@@ -0,0 +1,235 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
+import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.lang.Nullable;
+import java.util.NoSuchElementException;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+import java.util.Map;
+import java.util.HashMap;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+/**
+ * ProblemDetails
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-04-07T16:11:29.458034+02:00[Europe/Madrid]", comments = "Generator version: 7.21.0")
+public class ProblemDetails {
+
+  private JsonNullable<String> type = JsonNullable.<String>undefined();
+
+  private JsonNullable<String> title = JsonNullable.<String>undefined();
+
+  private JsonNullable<Integer> status = JsonNullable.<Integer>undefined();
+
+  private JsonNullable<String> detail = JsonNullable.<String>undefined();
+
+  private JsonNullable<String> instance = JsonNullable.<String>undefined();
+
+  public ProblemDetails type(String type) {
+    this.type = JsonNullable.of(type);
+    return this;
+  }
+
+  /**
+   * Get type
+   * @return type
+   */
+  
+  @Schema(name = "type", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("type")
+  public JsonNullable<String> getType() {
+    return type;
+  }
+
+  public void setType(JsonNullable<String> type) {
+    this.type = type;
+  }
+
+  public ProblemDetails title(String title) {
+    this.title = JsonNullable.of(title);
+    return this;
+  }
+
+  /**
+   * Get title
+   * @return title
+   */
+  
+  @Schema(name = "title", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("title")
+  public JsonNullable<String> getTitle() {
+    return title;
+  }
+
+  public void setTitle(JsonNullable<String> title) {
+    this.title = title;
+  }
+
+  public ProblemDetails status(Integer status) {
+    this.status = JsonNullable.of(status);
+    return this;
+  }
+
+  /**
+   * Get status
+   * @return status
+   */
+  
+  @Schema(name = "status", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("status")
+  public JsonNullable<Integer> getStatus() {
+    return status;
+  }
+
+  public void setStatus(JsonNullable<Integer> status) {
+    this.status = status;
+  }
+
+  public ProblemDetails detail(String detail) {
+    this.detail = JsonNullable.of(detail);
+    return this;
+  }
+
+  /**
+   * Get detail
+   * @return detail
+   */
+  
+  @Schema(name = "detail", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("detail")
+  public JsonNullable<String> getDetail() {
+    return detail;
+  }
+
+  public void setDetail(JsonNullable<String> detail) {
+    this.detail = detail;
+  }
+
+  public ProblemDetails instance(String instance) {
+    this.instance = JsonNullable.of(instance);
+    return this;
+  }
+
+  /**
+   * Get instance
+   * @return instance
+   */
+  
+  @Schema(name = "instance", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("instance")
+  public JsonNullable<String> getInstance() {
+    return instance;
+  }
+
+  public void setInstance(JsonNullable<String> instance) {
+    this.instance = instance;
+  }
+    /**
+    * A container for additional, undeclared properties.
+    * This is a holder for any undeclared properties as specified with
+    * the 'additionalProperties' keyword in the OAS document.
+    */
+    private Map<String, Object> additionalProperties;
+
+    /**
+    * Set the additional (undeclared) property with the specified name and value.
+    * If the property does not already exist, create it otherwise replace it.
+    */
+    @JsonAnySetter
+    public ProblemDetails putAdditionalProperty(String key, Object value) {
+        if (this.additionalProperties == null) {
+            this.additionalProperties = new HashMap<String, Object>();
+        }
+        this.additionalProperties.put(key, value);
+        return this;
+    }
+
+    /**
+    * Return the additional (undeclared) property.
+    */
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return additionalProperties;
+    }
+
+    /**
+    * Return the additional (undeclared) property with the specified name.
+    */
+    public Object getAdditionalProperty(String key) {
+        if (this.additionalProperties == null) {
+            return null;
+        }
+        return this.additionalProperties.get(key);
+    }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ProblemDetails problemDetails = (ProblemDetails) o;
+    return equalsNullable(this.type, problemDetails.type) &&
+        equalsNullable(this.title, problemDetails.title) &&
+        equalsNullable(this.status, problemDetails.status) &&
+        equalsNullable(this.detail, problemDetails.detail) &&
+        equalsNullable(this.instance, problemDetails.instance) &&
+    Objects.equals(this.additionalProperties, problemDetails.additionalProperties);
+  }
+
+  private static <T> boolean equalsNullable(JsonNullable<T> a, JsonNullable<T> b) {
+    return a == b || (a != null && b != null && a.isPresent() && b.isPresent() && Objects.deepEquals(a.get(), b.get()));
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(hashCodeNullable(type), hashCodeNullable(title), hashCodeNullable(status), hashCodeNullable(detail), hashCodeNullable(instance), additionalProperties);
+  }
+
+  private static <T> int hashCodeNullable(JsonNullable<T> a) {
+    if (a == null) {
+      return 1;
+    }
+    return a.isPresent() ? Arrays.deepHashCode(new Object[]{a.get()}) : 31;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ProblemDetails {\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    title: ").append(toIndentedString(title)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("    detail: ").append(toIndentedString(detail)).append("\n");
+    sb.append("    instance: ").append(toIndentedString(instance)).append("\n");
+    
+    sb.append("    additionalProperties: ").append(toIndentedString(additionalProperties)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(@Nullable Object o) {
+    return o == null ? "null" : o.toString().replace("\n", "\n    ");
+  }
+}
+
