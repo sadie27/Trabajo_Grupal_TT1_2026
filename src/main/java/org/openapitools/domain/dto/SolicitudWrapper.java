059956d7158f3f45172927a0acc78c475a22d29c
clara <clhernca@unirioja.es>
2026-04-21 15:36:52 +0200

    añadir SolicitudEntity y SolicitudWrapper

---
+++ src/main/java/org/openapitools/domain/dto/SolicitudWrapper.java
@@ -0,0 +1,18 @@
+package org.openapitools.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.openapitools.model.Solicitud;
+
+/**
+ * DTO Wrapper para manejar la estructura JSON que envía el cliente.
+ * El cliente envía: {"solicitud": {"cantidadesIniciales": [...], ...}}
+ * Este wrapper desempaqueta el objeto anidado.
+ */
+public class SolicitudWrapper {
+
+    @JsonProperty("solicitud")
+    private Solicitud solicitud;
+
+    public Solicitud getSolicitud() { return solicitud; }
+    public void setSolicitud(Solicitud solicitud) { this.solicitud = solicitud; }
+}
